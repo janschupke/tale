5a68814f2deb41983d52cd73bb6cbb8543b0e296
Jan Schupke <jan.schupke@gmail.com>
2016-01-31 17:01:27 +0100

    Configure JUnit.

---
+++ core/test/eu/janschupke/buddy/GlobalEventHandlerTest.java
@@ -0,0 +1,111 @@
+package eu.janschupke.buddy;
+
+import jdk.nashorn.internal.ir.annotations.Ignore;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Global event handler test class.
+ */
+public class GlobalEventHandlerTest {
+    @Test
+    public void thisAlwaysPasses()
+    {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    @Ignore
+    public void thisIsIgnored()
+    {
+    }
+//    @org.junit.Test
+//    public void testToggleUiDebug() throws Exception {
+//
+//    }
+//
+//    @org.junit.Test
+//    public void testTriggerMusic() throws Exception {
+//
+//    }
+//
+//    @org.junit.Test
+//    public void testAdjustMusicVolume() throws Exception {
+//
+//    }
+//
+//    @org.junit.Test
+//    public void testToggleMusic() throws Exception {
+//
+//    }
+//
+//    @org.junit.Test
+//    public void testToggleSound() throws Exception {
+//
+//    }
+//
+//    @org.junit.Test
+//    public void testToggleDebugRendering() throws Exception {
+//
+//    }
+//
+//    @org.junit.Test
+//    public void testShowMainMenu() throws Exception {
+//
+//    }
+//
+//    @org.junit.Test
+//    public void testShowGameMenu() throws Exception {
+//
+//    }
+//
+//    @org.junit.Test
+//    public void testStartNewGame() throws Exception {
+//
+//    }
+//
+//    @org.junit.Test
+//    public void testContinueGame() throws Exception {
+//
+//    }
+//
+//    @org.junit.Test
+//    public void testShowSettings() throws Exception {
+//
+//    }
+//
+//    @org.junit.Test
+//    public void testShowGraphicsMenu() throws Exception {
+//
+//    }
+//
+//    @org.junit.Test
+//    public void testShowAudioMenu() throws Exception {
+//
+//    }
+//
+//    @org.junit.Test
+//    public void testShowGameplayMenu() throws Exception {
+//
+//    }
+//
+//    @org.junit.Test
+//    public void testShowHotkeys() throws Exception {
+//
+//    }
+//
+//    @org.junit.Test
+//    public void testShowCredits() throws Exception {
+//
+//    }
+//
+//    @org.junit.Test
+//    public void testLeaveLevel() throws Exception {
+//
+//    }
+//
+//    @org.junit.Test
+//    public void testExitGame() throws Exception {
+//
+//    }
+}
