cfcd00d036386659dfead2b9176b9d37eeb2378f
Jan Schupke <jan.schupke@gmail.com>
2016-03-10 02:10:12 +0100

    Implement cave event logging.

--- core/src/eu/janschupke/tale/content/stage/level/cave/event/interaction/ville/VilleAggroInteractionEvent.java
+++ core/src/eu/janschupke/tale/content/stage/level/cave/event/interaction/ville/VilleAggroInteractionEvent.java
@@ -1,7 +1,9 @@
 package eu.janschupke.tale.content.stage.level.cave.event.interaction.ville;
 
+import eu.janschupke.tale.content.config.enumeration.tags.GameEventTags;
 import eu.janschupke.tale.framework.App;
 import eu.janschupke.tale.framework.event.InteractionEvent;
+import eu.janschupke.tale.framework.screen.BaseScreen;
 
 /**
  * Aggro book taking event.
@@ -25,7 +27,7 @@ public class VilleAggroInteractionEvent extends InteractionEvent {
 
     @Override
     protected void updateGameState() {
-
+        app.getGameLog().addEntry(GameEventTags.CAVE_INTERACTION_VILLE_AGGRO, ((BaseScreen) app.getScreen()).getTag());
     }
 
     @Override
